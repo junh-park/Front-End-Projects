10b215894cdbcfcfacf6fa55d98797f423e98e8d
junh-park <jhpar25@hotmail.com>
2021-10-22 20:26:20 +1100

    adding Todo list project

---
+++ Projects/To-do-list/js/events.js
@@ -0,0 +1,81 @@
+let addButton = document.getElementById("add-button");
+addButton.addEventListener("click", addToDoItem);
+
+function addToDoItem() {
+    let itemText = toDoEntryBox.value;
+    newToDoItem(itemText, false);
+}
+
+let toDoEntryBox = document.getElementById("todo-entry-box");
+let toDoList = document.getElementById("todo-list");
+
+function newToDoItem(itemText, completed) {
+    let toDoItem = document.createElement("li");
+    let toDoText = document.createTextNode(itemText);
+    toDoItem.appendChild(toDoText);
+
+    if (completed) {
+        toDoItem.classList.add("completed");
+    }
+
+    toDoList.appendChild(toDoItem);
+    toDoItem.addEventListener("dblclick", toggleToDoItemState);
+}
+
+function toggleToDoItemState() {
+    if(this.classList.contains("completed")) {
+        this.classList.remove("completed");
+    } else {
+        this.classList.add("completed");
+    }
+}
+
+let clearButton = document.getElementById("clear-completed-button");
+clearButton.addEventListener("click", clearCompletedToDoItems);
+function clearCompletedToDoItems() {
+    let completedItems = toDoList.getElementsByClassName("completed");
+
+    while(completedItems.length > 0) {
+        completedItems.item(0).remove();
+    }
+}
+
+let emptyButton = document.getElementById("empty-button");
+emptyButton.addEventListener("click", emptyList);
+function emptyList() {
+    let toDoItems = toDoList.children;
+    while(toDoItems.length > 0) {
+        toDoItems.item(0).remove();
+    }
+}
+
+let saveButton = document.getElementById("save-button");
+saveButton.addEventListener("click", saveList);
+
+function saveList() {
+    let toDos = [];
+    
+    for (let i = 0; i < toDoList.children.length; i++) {
+        let toDo = toDoList.children.item(0);
+
+        let toDoInfo = {
+            "task": toDo.innerText,
+            "completed": toDo.classList.contains("completed")
+        };
+        
+        toDos.push(toDoInfo);
+    }
+
+    localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
+function loadList() {
+    if(localStorage.getItem("toDos") != null) {
+        let toDos = JSON.parse(localStorage.getItem("toDos"));
+
+        for (let i = 0; i < toDos.length; i++) {
+            let toDo = toDos[i];
+            newToDoItem(toDo.task, todo.completed);            
+        }
+    }
+}
