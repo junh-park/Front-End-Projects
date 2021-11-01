a4a9a1aa38f43c7b2963e1b07c8eb8f25cdac4a4
junh-park <jhpar25@hotmail.com>
2021-11-01 18:17:43 +1100

    adding simple react app

---
+++ Projects/react-project/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./App.css";
+import Nav from "./components/Nav";
+import Tweets from "./components/Tweets";
+
+function App() {
+    return (
+        <div className='App'>
+            <h1>Hello React</h1>
+            <div className="home">
+                <Nav />
+                <Tweets />
+            </div>
+        </div>
+    )
+}
+
+export default App;
+
